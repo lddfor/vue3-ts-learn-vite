cdf5b17a8c5f5523ae21a8668fba605f5114a4f1
lidongdong03 <>
2022-11-20 20:37:16 +0800

    初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/   npm install --registry=http://registry.npmmirror.com
+// https://vitejs.dev/config/   33  npm install --registry=http://registry.npmmirror.com
 export default defineConfig({
   plugins: [vue()],
   resolve: {
